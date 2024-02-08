51fff067d62b6631faa0bec96b063e394c7fe6db
BrunoFares <faresbruno04@gmail.com>
2024-02-08 18:03:41 +0200

    Started working on the design of the website. I have currently put all the HTML code in the .html file, but I will transfer them to src/index.js when I am done with the CSS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("Eyup");
+// const origin = document.querySelector("div#content");
+// const title = document.createElement('h2');
+// const titleParag = document.createElement("p");
+
+// title.innerHTML = "Welcome to The Resting!";
+// titleParag.innerHTML = "A group of young individuals with an idea, which has now flourished into the comfiest place to relax, study, or chill with your friends!";
+
+// origin.appendChild(title);
+// origin.appendChild(titleParag);
+
+// const div = document.createAttribute("div");
+// div.classList.add("offer");
+
+// origin.appendChild(div);
