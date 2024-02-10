e6391ba77bb7fc7c86a575d288ad7fba080dab1c
BrunoFares <faresbruno04@gmail.com>
2024-02-10 15:05:10 +0200

    Fixed up and polished everything, moving on to the menu section.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import {renderHome} from './home.js';
 
-document.addEventListener('DOMContentLoaded', function() {
-    renderHome();
+const button = document.querySelectorAll('header button');
+
+button.forEach(btn => {
+    btn.addEventListener('mouseover', function() {
+        btn.style.backgroundColor = "rgb(80, 35, 0)";
+    })
 })
+
+button.forEach(btn => {
+    btn.addEventListener('mouseout', function() {
+        btn.style.backgroundColor = "rgb(95, 41, 0)";
+    })
+})
+
+button.forEach(btn => {
+    btn.addEventListener('mousedown', function() {
+        btn.style.backgroundColor = "rgb(48, 21, 0)";
+    })
+})
+
+button.forEach(btn => {
+    btn.addEventListener('mouseup', function() {
+        btn.style.backgroundColor = "rgb(95, 41, 0)";
+    })
+})
+
+renderHome();
