a63262274c17cf76b2dcc8bacdd56e21a12cea0d
BrunoFares <faresbruno04@gmail.com>
2024-02-20 16:13:28 +0200

    cleaned up the menu.js file with a loop that cycles through the menu categories that are placed in an array

--- src/menu.js
+++ src/menu.js
@@ -15,57 +15,34 @@ intro.appendChild(introParag);
 const menuDiv = document.createElement('div');
 menuDiv.classList.add('menu');
 
-const innerDiv1 = document.createElement('div');
-const h41 = document.createElement('h4');
-h41.textContent = 'Starters';
-const parag1 = document.createElement('p');
-parag1.innerHTML = 'French Fries<br>Onion Rings<br>Nachos<br>Hommos<br>Tabboule<br>Chicken Nuggets';
-const price1 = document.createElement('p');
-price1.innerHTML = '$3<br>$4<br>$7<br>$5<br>$5<br>$4';
+const menuTitles = ['Starters', 'Burgers', 'Pizzas', 'Crepes'];
+const menuOptions = [
+    'French Fries<br>Onion Rings<br>Nachos<br>Hommos<br>Tabboule<br>Chicken Nuggets',
+    'Hamburger<br>Chicken Burger<br>Hambigo<br>Burger Lovers<br>Tabburger',
+    'Bucciarratti<br>Margharetti<br>Decoco<br>Prosciutto e Funghi<br>Diavola<br>Forza Milan',
+    'Michel<br>Ribal<br>Chris<br>Kevin<br>Caramelo de Chocolate'
+]
+const menuPrices = [
+    '$3<br>$4<br>$7<br>$5<br>$5<br>$4',
+    '$6<br>$5<br>Priceless<br>$3<br>$4',
+    '$8<br>$8<br>$7<br>$10<br>$6<br>$15',
+    '$10<br>$10<br>$10<br>$10<br>$17'
+]
 
-innerDiv1.appendChild(parag1);
-innerDiv1.appendChild(price1);
-menuDiv.appendChild(h41);
-menuDiv.appendChild(innerDiv1);
+for (let i = 0; i < 4; i++) {
+    const innerDiv1 = document.createElement('div');
+    const h41 = document.createElement('h4');
+    h41.textContent = menuTitles[i];
+    const parag1 = document.createElement('p');
+    parag1.innerHTML = menuOptions[i];
+    const price1 = document.createElement('p');
+    price1.innerHTML = menuPrices[i];
 
-const innerDiv2 = document.createElement('div');
-const h42 = document.createElement('h4');
-h42.textContent = 'Burgers';
-const parag2 = document.createElement('p');
-parag2.innerHTML = 'Hamburger<br>Chicken Burger<br>Hambigo<br>Burger Lovers<br>Tabburger';
-const price2 = document.createElement('p');
-price2.innerHTML = '$6<br>$5<br>Priceless<br>$3<br>$4';
-
-innerDiv2.appendChild(parag2);
-innerDiv2.appendChild(price2);
-menuDiv.appendChild(h42);
-menuDiv.appendChild(innerDiv2);
-
-const innerDiv3 = document.createElement('div');
-const h43 = document.createElement('h4');
-h43.textContent = 'Pizzas';
-const parag3 = document.createElement('p');
-parag3.innerHTML = 'Bucciarratti<br>Margharetti<br>Decoco<br>Prosciutto e Funghi<br>Diavola<br>Forza Milan';
-const price3 = document.createElement('p');
-price3.innerHTML = '$8<br>$8<br>$7<br>$10<br>$6<br>$15';
-
-innerDiv3.appendChild(parag3);
-innerDiv3.appendChild(price3);
-menuDiv.appendChild(h43);
-menuDiv.appendChild(innerDiv3);
-
-const innerDiv4 = document.createElement('div');
-const h44 = document.createElement('h4');
-h44.textContent = 'Crepes';
-const parag4 = document.createElement('p');
-parag4.innerHTML = 'Michel<br>Ribal<br>Chris<br>Kevin<br>Caramelo de Chocolate';
-const price4 = document.createElement('p');
-price4.innerHTML = '$10<br>$10<br>$10<br>$10<br>$17';
-
-innerDiv4.appendChild(parag4);
-innerDiv4.appendChild(price4);
-menuDiv.appendChild(h44);
-menuDiv.appendChild(innerDiv4);
+    innerDiv1.appendChild(parag1);
+    innerDiv1.appendChild(price1);
+    menuDiv.appendChild(h41);
+    menuDiv.appendChild(innerDiv1);
+}
 
 function renderMenu() {
     origin.appendChild(intro);
